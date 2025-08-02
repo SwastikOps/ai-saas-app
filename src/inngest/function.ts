9c42a4812530996fdc2eec795f14b4ac1ab89a10
SWASTIK-WD <swastikranjanmandal@gmail.com>
2025-08-02 16:58:02 +0530

    Inngest Server and Background Jobs

---
+++ src/inngest/function.ts
@@ -0,0 +1,12 @@
+import { inngest } from "./client";
+
+export const helloWorld = inngest.createFunction(
+  { id: "hello-world" },
+  { event: "test/hello.world" },
+  async ({ event, step }) => {
+    await step.sleep("wait-a-moment", "30s"); //imagine this is a download step
+    await step.sleep("wait-a-moment", "10s"); //imagine this is a transcript step
+    await step.sleep("wait-a-moment", "5s");  //imagine this is a summary step
+    return { message: `Hello ${event.data.email}!` };
+  },
+);
